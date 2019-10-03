1cd5476c569e38d878c22600d63c23c5d4431c44
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2019-10-02 21:42:41 -0400

    Added forgotMyPasswordFE, fixed issues in signInFE,  added logic for FE

--- app/src/main/java/com/example/soswedding/ui/forgotPassword/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/soswedding/ui/forgotPassword/ForgotPasswordFragment.java
@@ -1,5 +1,6 @@
 package com.example.soswedding.ui.forgotPassword;
 
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -11,12 +12,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.soswedding.R;
+import com.example.soswedding.ui.SignIn.SignInFragment;
+import com.example.soswedding.ui.SignIn.SignInViewModel;
 
 public class ForgotPasswordFragment extends Fragment {
 
     private ForgotPasswordViewModel mViewModel;
+    private EditText emailInput;
+    private Button resetBtn;
+    private Button cancelBtn;
 
     public static ForgotPasswordFragment newInstance() {
         return new ForgotPasswordFragment();
@@ -25,7 +33,30 @@ public class ForgotPasswordFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.forgot_password_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.forgot_password_fragment, container, false);
+        initializeVariables(rootView);
+        setupOnClickEvents();
+        return rootView;
+    }
+
+    private void initializeVariables(View rootView) {
+        emailInput = rootView.findViewById(R.id.emailForgotPasswordInput);
+        resetBtn = rootView.findViewById(R.id.resetBtn);
+        cancelBtn = rootView.findViewById(R.id.cancelBtn);
+    }
+
+    private void setupOnClickEvents() {
+        setupCancelOnClickEvent();
+
+    }
+
+    private void setupCancelOnClickEvent() {
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCancelClicked();
+            }
+        });
     }
 
     @Override
@@ -34,5 +65,25 @@ public class ForgotPasswordFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(ForgotPasswordViewModel.class);
         // TODO: Use the ViewModel
     }
+
+    public void onCancelClicked(){
+        Fragment fragment = null;
+        try {
+            fragment = SignInFragment.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, fragment)
+                .commit();
+
+    }
+    public void onResetClicked(View view){
+        //TODO: implementation to come
+
+    }
+
+
 
 }
