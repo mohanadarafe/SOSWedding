f6553a1d7b1830b7830830b863f924e98dc1ed3d
Christophe <cbahenduzi@gmail.com>
2019-11-23 20:56:12 -0500

    Merge branches 'chris' and 'master' of https://github.com/mohanadarafe/SOSWedding into chris
    # Conflicts:
    #	app/src/main/res/layout/register_fragment.xml
    #	local.properties

--- app/src/main/java/com/example/soswedding/ui/offers/OffersFragment.java
+++ app/src/main/java/com/example/soswedding/ui/offers/OffersFragment.java
@@ -29,16 +29,9 @@ public class OffersFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        offersViewModel =
-                ViewModelProviders.of(this).get(OffersViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_offers, container, false);
-        final TextView textView = root.findViewById(R.id.text_offers);
-        offersViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
