1db9548355f95b88d962fc3f00f291fb9806edf3
Christophe Bahenduzi <46238950+csbduzi@users.noreply.github.com>
2019-11-26 14:52:01 -0500

    Merge branch 'master' of https://github.com/mohanadarafe/SOSWedding into 'chrisChacal'
    # Conflicts:
    #	local.properties

--- app/src/main/java/com/example/soswedding/ui/Offer/OffersViewModel.java
+++ app/src/main/java/com/example/soswedding/ui/Offer/OffersViewModel.java
@@ -14,6 +14,7 @@ import static com.example.soswedding.service.BidService.declineBid;
 
 public class OffersViewModel extends ViewModel {
 
+    // TODO: Implement the ViewModel
     public OffersViewModel() {
 
     }
