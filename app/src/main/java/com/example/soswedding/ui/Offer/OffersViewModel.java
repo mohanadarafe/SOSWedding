79935e22477b67d27a4e7921c4998640e549a9c0
Alain D. Jobs <33582344+AlainJobs@users.noreply.github.com>
2019-11-25 22:27:17 -0500

    Merge branches 'alain' and 'master' of https://github.com/mohanadarafe/SOSWedding into alain
    # Conflicts:
    #	app/src/main/java/com/example/soswedding/model/Offer.java
    #	app/src/main/java/com/example/soswedding/ui/Offer/OffersFragment.java
    #	app/src/main/java/com/example/soswedding/ui/all_offers/AllOffersFragment.java
    #	app/src/main/java/com/example/soswedding/ui/all_offers/AllOffersViewModel.java
    #	app/src/main/java/com/example/soswedding/ui/all_offers/OffersAdapter.java
    #	local.properties

--- app/src/main/java/com/example/soswedding/ui/Offer/OffersViewModel.java
+++ app/src/main/java/com/example/soswedding/ui/Offer/OffersViewModel.java
@@ -1,9 +1,20 @@
 package com.example.soswedding.ui.Offer;
 
+import android.content.Context;
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
+
+import com.example.soswedding.Interface.VolleyCallback;
+import com.example.soswedding.model.Singleton;
+
+import static com.example.soswedding.service.BidService.postBid;
 
 public class OffersViewModel extends ViewModel {
 
     // TODO: Implement the ViewModel
+    public OffersViewModel() {
+
+    }
 
 }
