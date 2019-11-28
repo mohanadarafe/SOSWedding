addc4fa2f6595f5d0969f33c7572dd56744d50ee
Hambrsoom <hampico1234@gmail.com>
2019-11-28 16:41:31 -0500

    test cases

--- app/src/main/java/com/example/soswedding/ui/Request/RequestViewModel.java
+++ app/src/main/java/com/example/soswedding/ui/Request/RequestViewModel.java
@@ -7,6 +7,9 @@ import com.example.soswedding.Interface.VolleyCallback;
 import com.example.soswedding.model.Request;
 import com.example.soswedding.model.User;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import androidx.lifecycle.ViewModel;
 
 import static com.example.soswedding.service.BidService.postBid;
@@ -17,17 +20,39 @@ public class RequestViewModel extends ViewModel {
 
     public void postBidModel(Context context, String amount, Request request, User user){
         if(!verifyCoupleUserType(user) && userInputValidation(amount)){
-            sendPostBidRequest(context,amount,request,user);
+            JSONObject bidObject = createBidObject(amount,request,user);
+            sendPostBidRequest(context,bidObject);
         }
     }
 
-    public void sendPostBidRequest(Context context,String amount,Request request, User user){
-        postBid(context,amount,request, user, new VolleyCallback() {
+    public void sendPostBidRequest(Context context,JSONObject bidObject){
+        postBid(context,bidObject, new VolleyCallback() {
             @Override
             public void onSuccess(String result) {
                 Log.e(result,"Successfully posted bid");
             }
         });
+    }
+
+
+    public JSONObject createBidObject(String amount, Request request, User user){
+        try {
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("amount", Double.parseDouble(amount));
+            jsonBody.put("message", "Hello WORLD");
+            jsonBody.put("status", "PENDING");
+            jsonBody.put("providerUuid", user.getUuid());
+            jsonBody.put("coupleUuid", request.getuID());
+            jsonBody.put("requestId", request.getId());
+            jsonBody.put("requestTitle", request.getTitle());
+            jsonBody.put("companyName", user.getCompanyName());
+            return jsonBody;
+        }
+        catch( JSONException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public boolean verifyCoupleUserType (User user){
